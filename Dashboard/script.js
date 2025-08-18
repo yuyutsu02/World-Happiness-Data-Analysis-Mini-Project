bd6a0ca55868f14ce44c759b7449a4106df1e1ab
Yuyutsu <yuyutsu02@gmail.com>
2025-08-18 19:01:35 +0530

    lower half to upper half , all countries

---
+++ Dashboard/script.js
@@ -0,0 +1,9 @@
+//selecting elements
+container=document.querySelector('.container');
+
+
+//welcome screen
+welcome = document.createElement('div');
+welcome.classList.add('welcome');
+container.append(welcome);
+
